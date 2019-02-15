02c2a2cbc744a45d114f22a5fe3237f75be696b5
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-15 18:04:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,14 @@
 # Write your code here.
 def line(katz_deli)
+  line_str = "The line is currently: "
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0 
-    
+    katz_deli.each_with_index do |name, index|
+      line_str += "#{index + 1}. #{name}"
+    end
+  end
+  line_str
 end
 
 def take_a_number(katz_deli, new_customer)
