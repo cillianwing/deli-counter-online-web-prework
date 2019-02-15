f1d9044c97731079abf04941b6e0d69055ee5fec
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-15 18:08:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 def line(katz_deli)
-  line_str = "The line is currently: "
+  line_str = "The line is currently:"
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0 
     katz_deli.each_with_index do |name, index|
-      line_str += "#{index + 1}. #{name}"
+      line_str += " #{index + 1}. #{name}"
     end
   end
   line_str
