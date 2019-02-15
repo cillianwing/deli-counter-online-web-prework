83ccb40c0f781acdafa3bb37dc5af80089d3d830
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-15 18:02:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
-  elsif 
+  elsif katz_deli.length > 0 
+    
 end
 
 def take_a_number(katz_deli, new_customer)
